f7c8876d85210b737c846d64f4c34735a6267d08
TheGoat-glitch <69991537+TheGoat-glitch@users.noreply.github.com>
2024-01-20 18:22:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/minigames')
+def minigames():
+    return render_template('minigames.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
 
 # ... other routes if needed ...
 
 if __name__ == '__main__':
     # Serve static files (CSS, JS, images, etc.)
-    app.static_folder = 'static'
-    
     app.run(debug=True)
 
